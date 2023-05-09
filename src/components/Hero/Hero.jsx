b00d50da92362ddedfa554efc7da8b4fb086eab2
Achrafcodes <essoussygamer@gmail.com>
2023-05-09 19:37:16 +0100

    done

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,18 +1,42 @@
 import React from 'react';
 import NextJs from '../../assets/undraw_next_js_-8-g5m (1).svg';
+import { useState, useEffect } from 'react';
+import Libraries from '../../assets/undraw_javascript_frameworks_-8-qpc.svg';
 function Hero() {
+  let images = [NextJs, Libraries];
+  let Texts = [
+    'Power your web presence with WebT',
+    'Experienced team of developers with a proven track record of success'
+  ];
+  let [currentImage, setCurrentImage] = useState(0);
+  let [currentText, SetCurrentText] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImage(prevImage => (prevImage + 1) % images.length);
+      SetCurrentText(prevText => (prevText + 1) % Texts.length);
+    }, 10000);
+
+    return () => clearInterval(interval); // Clear interval on component unmount
+  }, []); // Empty dependency array to run the effect only once
+
   return (
     <div className="Hero">
       <div className="left-container">
-        <h1 className="headding">
-          Power Up Your Web Presence with{' '}
-          <span className="headerSpan">
-            Web<span>T</span>
-          </span>
-        </h1>
+        {Texts.map((text, index) => (
+          <h1 className={index === currentText ? 'active' : ''} key={index}>
+            {text}
+          </h1>
+        ))}
       </div>
       <div className="right-container">
-        <img src={NextJs} alt="" />
+        {images.map((image, index) => (
+          <img
+            key={index}
+            src={image}
+            alt="Slider"
+            className={index === currentImage ? 'active' : ''}
+          />
+        ))}
       </div>
     </div>
   );
